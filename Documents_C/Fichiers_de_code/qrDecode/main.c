f81c986c38eb10b9aec9772791488e2c13074417
Taumah <ttresgots@gmail.com>
2020-04-04 22:34:32 +0200

    decode works , returning a long string

--- Documents_C/Fichiers_de_code/qrDecode/main.c
+++ Documents_C/Fichiers_de_code/qrDecode/main.c
@@ -2,23 +2,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "tests/dbgutil.h"
+#include "tests/inspect.h"
+char* qrDecode(void);
+
 int main(void){
+	char* result = qrDecode();
+
+	if(result != NULL){
+		printf("\n voila \n%s", result);
+		free(result);
+	}
 	
+	
+	return 0;
+}
+
+
+char* qrDecode(){
+	int num_codes;
+	int i;
 	struct quirc *qr;
+
+	char *foundText = NULL;
 
 	qr = quirc_new();
 	if (!qr) {
 		printf("Failed to allocate memory");
 		abort();
 	}
+	quirc_begin(qr, NULL , NULL);
 
-	if (quirc_resize(qr, 640, 480) < 0) {
-    perror("Failed to allocate video memory");
-    abort();
-}
+	int status = load_png(qr, "recent.png");
 
-	quirc_destroy(qr);
-	printf("end\n");
+
+	quirc_end(qr);
+	struct quirc_code code;
+	struct quirc_data data;
+	quirc_decode_error_t err;
+
+	quirc_extract(qr, i, &code);
+	err = quirc_decode(&code, &data);
+	if (err) {
+		printf("  Decoding FAILED: %s\n", quirc_strerror(err));
+		quirc_destroy(qr);
+		return NULL;
+	} else {
+		printf("  Decoding successful:\n");
+		printf("    Payload: %s\n", data.payload);
+
+		foundText = malloc(sizeof(char) * data.payload_len);
+		strcpy(foundText,data.payload);
+		return foundText;
+	}
 	
-	return 0;
+	printf("end\n");
 }
