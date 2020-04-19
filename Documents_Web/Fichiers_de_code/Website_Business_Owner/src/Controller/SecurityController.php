f3aa3ef95c1573f484f5fdd813e7c9e9b7ce6979
antoineThiel <antoine.thiel@hotmail.fr>
2020-04-19 15:23:22 +0200

    Espace connecté front finito

--- Documents_Web/Fichiers_de_code/Website_Business_Owner/src/Controller/SecurityController.php
+++ Documents_Web/Fichiers_de_code/Website_Business_Owner/src/Controller/SecurityController.php
@@ -33,4 +33,22 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function userLogin(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('target_path');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
 }
