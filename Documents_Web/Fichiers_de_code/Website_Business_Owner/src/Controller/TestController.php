66b04ffb2ae8c257b6b2a0d6a35a1fa0a55f4b08
antoineThiel <antoine.thiel@hotmail.fr>
2020-06-06 17:43:27 +0200

    Mobile App

--- Documents_Web/Fichiers_de_code/Website_Business_Owner/src/Controller/TestController.php
+++ Documents_Web/Fichiers_de_code/Website_Business_Owner/src/Controller/TestController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 use App\Repository\CustomerRepository;
+use App\Repository\FranchiseeRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -54,6 +55,29 @@ class TestController extends AbstractController
         }
 
         $serializedResponse = $serializer->serialize($response, 'json');
+        return new JsonResponse($serializedResponse, 200, [], true);
+    }
+
+
+    /**
+     * @Route("/getfranchisee", name="getFranchisee", methods={"GET"})
+     */
+    public function getFranchisee(SerializerInterface $serializer) : JsonResponse
+    {
+        $repo = $this->getDoctrine()->getManager()->getRepository('App\Entity\Franchisee');
+        $franchisees = $repo->findAll();
+        $finalFranchisee = [];
+        $i = 0;
+        if ($franchisees !== null){
+            foreach ($franchisees as $franchisee)
+            {
+                $finalFranchisee[$i]['id'] = $franchisee->getId();
+                $finalFranchisee[$i]['lastName'] = $franchisee->getLastName();
+                $finalFranchisee[$i]['firstName'] = $franchisee->getFirstName();
+                $i++;
+            }
+        }
+        $serializedResponse = $serializer->serialize($finalFranchisee, 'json');
         return new JsonResponse($serializedResponse, 200, [], true);
     }
 
