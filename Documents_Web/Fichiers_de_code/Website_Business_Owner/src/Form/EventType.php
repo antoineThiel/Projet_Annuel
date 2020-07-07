928e6af8e2655cd682f83c3014b604f5db66dbcd
Océane <46958330+Eliwena@users.noreply.github.com>
2020-07-07 23:20:39 +0200

    Multiple choice | date

--- Documents_Web/Fichiers_de_code/Website_Business_Owner/src/Form/EventType.php
+++ Documents_Web/Fichiers_de_code/Website_Business_Owner/src/Form/EventType.php
@@ -6,15 +6,12 @@ namespace App\Form;
 use App\Entity\Event;
 use App\Entity\FranchiseeArticle;
 use App\Entity\FranchiseeMenu;
-use App\Entity\MenuToArticle;
 use App\Repository\FranchiseeArticleRepository;
 use App\Repository\FranchiseeMenuRepository;
 use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -25,14 +22,17 @@ class EventType extends AbstractType
         $builder
             ->add('startDate', DateType::class, [
                 'format' => 'dd/MM/yyyy',
-                'html5' => false
+                'years' => range(date('Y'), date('Y')+10),
+                'months' => range(date('m'), 12),
             ])
+
             ->add('endDate', DateType::class, [
                 'format' => 'dd/MM/yyyy',
-                'html5' => false
+                'html5' => false,
+                'years' => range(date('Y'), date('Y')+10),
+                'months' => range(date('m'), 12),
             ])
             ->add('articles', EntityType::class, [
-                'placeholder' => 'Choisir un Article',
                 'class' => FranchiseeArticle::class,
                 'choice_label' => 'name',
                 'expanded'=> true,
@@ -43,7 +43,6 @@ class EventType extends AbstractType
                 }
             ])
             ->add('menues', EntityType::class, [
-                'placeholder' => 'Choisir un menue',
                 'class' => FranchiseeMenu::class,
                 'choice_label' => 'name',
                 'expanded'=> true,
